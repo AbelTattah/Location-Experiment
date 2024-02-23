22a45a04fca6df8612c6eb0afe6cab2acaf4420b
AbelTattah <abeltattah@gmail.com>
2024-02-23 15:54:44 +0000

    update: Edit Homepage

--- record.js
+++ record.js
@@ -1,14 +1,12 @@
-import { post } from 'axios';
-
-const Record = async() => {
-    try {
-        const ip = await post('http://localhost:4100/record',
-        {
-            ip: document.getElementById("ip").value
-        });
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+fetch('https://api.example.com/posts', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ title: 'My Post', content: 'Hello, world!' })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 
 
